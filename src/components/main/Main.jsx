85c17265ec7f59a841d1f96a6beab8c44df33f26
Kraig Hebert <kraighebert@gmail.com>
2022-07-15 18:47:27 -0400

    messed with chess api

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,17 @@
 // import basics
 import React from 'react';
 import './main.css';
+import ChessWebAPI from 'chess-web-api/src/chess-web-api';
 
 const Main = () => {
+  const chessAPI = new ChessWebAPI();
+  const playerData = (username, year, month) => {
+    chessAPI.getPlayerMultiGamePGN(username, year, month).then((response) => {
+      console.log('Player Profile', response.body);
+    });
+  };
+
+  playerData('PinsNicety', 2022, 6);
   return <div>Main</div>;
 };
 
